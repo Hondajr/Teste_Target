a19b378965d889be37879958c8610a7a454b70fa
Hondajr <124377744+Hondajr@users.noreply.github.com>
2023-03-09 15:52:58 -0300

    Add files via upload

--- Inversão de String.py
+++ Inversão de String.py
@@ -1,5 +1,9 @@
+
 entrada = input("Digite sua String: ")
 
+# Jeito Python de realizar:
+# O python identifica a string como um array de chars
+# sendo assim eu salvo um novo array de chars, só que com step -1
 saida = entrada[::-1]
 
 print(saida)
--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,20 @@
+
+# Entrada de dados, salvando-a como Integer
 voltas = int(input("Informe um número inteiro: "))
+
+# Declarando a variavel de saída
 total = list("")
+
+# Loop com voltas, lembrando de realizar o +1, pois o python não executa a última volta em seu For
 for i in range(0,voltas+1):
+    #Teste para os 2 primeiros elementos, tendo em vista que são unicos
     if i == 0 or i==1:
+        # Funcao append, adiciona um elemento a lista
         total.append(i)
     else:
         total.append(total[i-1]+total[i-2])
+
+# Teste para verificar se o valor informado está na lista:
 if voltas in total:
     print(f"O valor informado, {voltas}, pertence a sequencia!")
 else:
